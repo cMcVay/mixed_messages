d99bd674158f15fff8025192b2d3c36ba6838709
cMcVay <connor.mcvay@yahoo.com>
2021-07-12 15:07:13 -0400

    Object of message parts created with arrays nested

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-weatherReport = () => {
-    let temp = 5;
-};
+
+//arrays for message parts
+const weather = {
+    temperature: ["cold", "hot", "warm", "scorching", "freezing", "cool", "chilly"],
+    precipitation: ["rain", "snow", "sleet", "hale", "frezzing rain", "acid rain", "meatballs"],
+    chance: ["high", "low", "moderate", "very high", "near zero"]
+}
+
