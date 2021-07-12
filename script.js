022562fe75490d1b09144191be9802c5de301b55
cMcVay <connor.mcvay@yahoo.com>
2021-07-12 15:51:53 -0400

    added console.log

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const weather = {
     temperature: ["cold", "hot", "warm", "scorching", "freezing", "cool", "chilly"],
     chance: ["high", "low", "moderate", "very high", "near zero"],
-    precipitation: ["rain", "snow", "sleet", "hale", "frezzing rain", "acid rain", "meatballs"]
+    precipitation: ["rain", "snow", "sleet", "hale", "freezing rain", "acid rain", "meatballs"]
     
 };
 
@@ -20,13 +20,16 @@ for (let weatherType in weather) {
     let arrayIndex = generateARandomInteger(weather[weatherType].length);
     switch(weatherType) {
         case 'temperature':
-            weatherReport.push(`It should be a ${weather[temperature][arrayIndex]} day today`)
+            weatherReport.push(`It should be a ${weather[weatherType][arrayIndex]} day today`)
             break
         case 'chance':
-            weatherReport.push(` with a ${weather[chance][arrayIndex]} chance of`)
+            weatherReport.push(` with a ${weather[weatherType][arrayIndex]} chance of`)
             break
         case 'precipitation':
-            weatherReport.push(` ${weather[precipitation][arrayIndex]}. Hope you enjoy your day at the beach!`)
+            weatherReport.push(` ${weather[weatherType][arrayIndex]}. Hope you enjoy your day at the beach!`)
             break
     }
 }
+
+//console log results
+console.log(weatherReport.join(''));
