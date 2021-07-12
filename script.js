98b004140a20720549f5d005a27e9473146bae49
cMcVay <connor.mcvay@yahoo.com>
2021-07-12 15:24:00 -0400

    generate random function, weather report array

--- script.js
+++ script.js
@@ -4,5 +4,17 @@ const weather = {
     temperature: ["cold", "hot", "warm", "scorching", "freezing", "cool", "chilly"],
     precipitation: ["rain", "snow", "sleet", "hale", "frezzing rain", "acid rain", "meatballs"],
     chance: ["high", "low", "moderate", "very high", "near zero"]
+};
+
+//generate random number with number as input
+const generateARandomInteger = (number) => {
+    return Math.floor(Math.random()*number);
 }
 
+//array for message components
+let weatherReport = [];
+
+//iterate through weather and add messages
+for (let weatherType in weather) {
+
+}
