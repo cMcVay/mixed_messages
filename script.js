caf101d254a5eec12983e82769e06e6a54ac9c48
cMcVay <connor.mcvay@yahoo.com>
2021-07-12 15:46:23 -0400

    iteration through weather

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 //arrays for message parts
 const weather = {
     temperature: ["cold", "hot", "warm", "scorching", "freezing", "cool", "chilly"],
-    precipitation: ["rain", "snow", "sleet", "hale", "frezzing rain", "acid rain", "meatballs"],
-    chance: ["high", "low", "moderate", "very high", "near zero"]
+    chance: ["high", "low", "moderate", "very high", "near zero"],
+    precipitation: ["rain", "snow", "sleet", "hale", "frezzing rain", "acid rain", "meatballs"]
+    
 };
 
 //generate random number with number as input
@@ -16,5 +17,16 @@ let weatherReport = [];
 
 //iterate through weather and add messages
 for (let weatherType in weather) {
-
+    let arrayIndex = generateARandomInteger(weather[weatherType].length);
+    switch(weatherType) {
+        case 'temperature':
+            weatherReport.push(`It should be a ${weather[temperature][arrayIndex]} day today`)
+            break
+        case 'chance':
+            weatherReport.push(` with a ${weather[chance][arrayIndex]} chance of`)
+            break
+        case 'precipitation':
+            weatherReport.push(` ${weather[precipitation][arrayIndex]}. Hope you enjoy your day at the beach!`)
+            break
+    }
 }
